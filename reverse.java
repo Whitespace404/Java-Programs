56c6a328db8338e1d9d69f583a1fcf31c73e7626
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-16 18:37:27 +0100

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,10 @@
+// Write a program to print the numbers 
+// from 1 to 10 in reverse order.
+
+public class reverse {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
