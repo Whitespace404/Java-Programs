ddfceed29897c93331246c18bdfec0d3563fa5b1
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-16 18:41:15 +0100

    Create all_even.java

---
+++ all_even.java
@@ -0,0 +1,10 @@
+// Write a program to print all even numbers between
+// 1 and 20.
+
+public class all_even {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 20; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
