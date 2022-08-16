c271f01f869718cff72f658fdaf93082ce0dc2c1
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-16 18:31:24 +0100

    Add files via upload

---
+++ menu_driven.java
@@ -0,0 +1,32 @@
+// Write a menu driven program to calculate the
+// area and circumference of a circle.
+
+import java.util.Scanner;
+
+public class menu_driven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Would you like to calculate the area (1) or the circumference (2)? ");
+
+        int choice = input.nextInt();
+        double pi = 3.1415926535897;
+
+        switch (choice) {
+            case 1:
+            System.out.println("Radius: ");
+            int radius = input.nextInt();
+
+            double area = pi * radius * radius;
+            System.out.println("The area of the circle is " + area);
+            break;
+
+            case 2:
+            System.out.println("Radius: ");
+            int radius_c = input.nextInt();
+
+            double circumference = 2 * pi * radius_c;
+            System.out.println("The circumference is " + circumference);
+            break;
+        }
+    }
+}
