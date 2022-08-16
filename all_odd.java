aa735fa5deae86c4ebb65f01d6850bc694bb0ded
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-16 18:40:18 +0100

    Create all_odd.java

---
+++ all_odd.java
@@ -0,0 +1,10 @@
+// Write a program to print all odd numbers between
+// 1 and 20.
+
+public class all_odd {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
