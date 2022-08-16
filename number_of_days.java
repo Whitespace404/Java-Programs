50ba551699c41b3935acb7f293ca25476f8b202f
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-16 18:14:19 +0100

    Add files via upload

---
+++ number_of_days.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class number_of_days {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String month = input.nextLine();
+
+        switch (month) {
+            case "January":
+            case "May":
+            case "March":
+            case "August":
+            case "October":
+            case "December":
+            System.out.println("31 days");
+            break;
+
+            case "February":
+            System.out.println("28 days");
+            break;
+
+            case "April":
+            case "June":
+            case "September":
+            case "November":
+            System.out.println("30 days");
+            break;
+        }
+    }
+}
