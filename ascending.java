48e4920f24fbf343d2b4b469b9ff455bb6686a28
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-16 18:38:59 +0100

    Create ascending.java

---
+++ ascending.java
@@ -0,0 +1,10 @@
+// Write a program to print the numbers between
+// 1 and 10 in ascending order.
+
+public class ascending {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
