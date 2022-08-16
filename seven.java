66c7e10edf0e4455b274468157bef565770e753b
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-16 18:42:50 +0100

    Create seven.java

---
+++ seven.java
@@ -0,0 +1,10 @@
+// Write a program to print all numbers divisible
+// by 7 between 1 and 100 using a for loop.
+
+public class seven {
+    public static void main(String[] args) {
+        for (int i = 7; i <= 100; i+=7) {
+            System.out.println(i);
+        }
+    }
+}
