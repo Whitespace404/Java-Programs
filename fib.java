b3579c5ec6156f85a92ff0ae1ca48a3e9deeb0c6
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-17 13:30:34 +0100

    Create fib.java

---
+++ fib.java
@@ -0,0 +1,17 @@
+// Write a program to print the first 10 numbers
+// of the Fibbonaci series using a for loop
+
+public class fib {
+    public static void main(String args[]) {
+      int n1 = 0;	     
+      int n2 = 1;
+
+    	for (int iterations = 1; iterations < 10; iterations++) {
+	    System.out.println(n1);
+            int sum = n1 + n2;
+
+            n1 = n2;
+            n2 = sum;
+	    }
+    }
+}
