49893e380fe0a815c6697d07297ac6957d3e4cbd
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-17 13:34:00 +0100

    Update fib.java

--- fib.java
+++ fib.java
@@ -2,16 +2,16 @@
 // of the Fibbonaci series using a for loop
 
 public class fib {
-	public static void main(String args[]) {
-		int n1 = 0;
-		int n2 = 1;
+    public static void main(String args[]) {
+	int n1 = 0;
+	int n2 = 1;
 
-		for (int iterations = 1; iterations < 10; iterations++) {
-		System.out.println(n1);
-			int sum = n1 + n2;
+	for (int iterations = 1; iterations < 10; iterations++) {
+	    System.out.println(n1);
+	    int sum = n1 + n2;
 
-			n1 = n2;
-			n2 = sum;
-		}
+	    n1 = n2;
+	    n2 = sum;
 	}
+    }
 }
