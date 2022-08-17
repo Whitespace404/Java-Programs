4a5ccb009d54d901be2945d4301b86ed0df94b2e
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-08-17 15:29:29 +0100

    Create plusoRRminus.java

---
+++ plusoRRminus.java
@@ -0,0 +1,18 @@
+/* Write a program to input a number and check whether
+it is a negative number or a positive number. */
+
+import java.util.Scanner;
+
+public class plusoRRminus {
+  public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+    int number = input.nextInt();
+    
+    if (number > 0) {
+      System.out.println(number + " is positive.")
+    }
+    else if (number < 0) {
+      System.out.println(number + "is negative.");
+    }
+  }
+}
