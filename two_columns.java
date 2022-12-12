aa187219c2328465c13de9ac3674baa7690d0359
Whitespace404 <63506767+Whitespace404@users.noreply.github.com>
2022-12-12 12:33:50 +0000

    Update two_columns.java

--- two_columns.java
+++ two_columns.java
@@ -1,9 +1,9 @@
-/* Write a program to print numbers 1 to 9 and
-9 to 1 in two columns. */
+/* Write a program to print numbers 1 to 10 and
+10 to 1 in two columns. */
 
 public class two_columns {
     public static void main(String args[]) {
-        for (int a=9, b=1; b<=9; a--, b++) {
+        for (int a=10, b=1; b<=10; a--, b++) {
             System.out.println(a + " " + b);
         }
     }
